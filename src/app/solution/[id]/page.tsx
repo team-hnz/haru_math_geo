affa5dcbd7b4f822aac8762d35279ff5c4271be4
zevo1 <bsiku3622@naver.com>
2024-07-24 17:07:29 +0900

    z

--- src/app/solution/[id]/page.tsx
+++ src/app/solution/[id]/page.tsx
@@ -3,74 +3,66 @@
 import { lrstrip, rtrim, ltrim } from "../../../utils/types/StringUtil";
 import { useState, useEffect } from "react";
 
-import { Tooltip } from "react-tooltip";
 import "../../../styles/Solution.css";
 
-import Toggle from "react-toggle";
-import ReactModal from "react-modal";
-import {
-  Item,
-  ItemParams,
-  Menu,
-  Separator,
-  Submenu,
-  useContextMenu,
-} from "react-contexify";
-import { useSnackbar } from "notistack";
 import { COORD } from "../../../utils/types/DataUtil";
-import { JSXGraphCanvas, JSXGraph } from "../../../shared/components/JsxGraph";
+import {
+  JSXGraphCanvas,
+  JSXGraph,
+  JSXGraphLib,
+} from "../../../shared/components/JsxGraph";
 import { getRandomIntInclusive } from "../../../utils/types/MathUtil";
 
-const test_file = `
-==========================
- * From theorem premises:
-A B C H G1 G2 G O : Points
-BH ⟂ AC [00]
-AH ⟂ BC [01]
-G_1,C,B are collinear [02]
-G_1B = G_1C [03]
-C,A,G_2 are collinear [04]
-G_2C = G_2A [05]
-G_1,A,G are collinear [06]
-G_2,B,G are collinear [07]
-OB = OC [08]
-OA = OB [09]
+// const test_file = `
+// ==========================
+//  * From theorem premises:
+// A B C H G1 G2 G O : Points
+// BH ⟂ AC [00]
+// AH ⟂ BC [01]
+// G_1,C,B are collinear [02]
+// G_1B = G_1C [03]
+// C,A,G_2 are collinear [04]
+// G_2C = G_2A [05]
+// G_1,A,G are collinear [06]
+// G_2,B,G are collinear [07]
+// OB = OC [08]
+// OA = OB [09]
 
- * Auxiliary Constructions:
-H2 G3 : Points
-CA ⟂ H_2B [10]
-C,A,H_2 are collinear [11]
-A,B,G_3 are collinear [12]
-G_3A = G_3B [13]
+//  * Auxiliary Constructions:
+// H2 G3 : Points
+// CA ⟂ H_2B [10]
+// C,A,H_2 are collinear [11]
+// A,B,G_3 are collinear [12]
+// G_3A = G_3B [13]
 
- * Proof steps:
-001. G_1,C,B are collinear [02] & G_1B = G_1C [03] ⇒  G_1 is midpoint of CB [14]
-002. C,A,G_2 are collinear [04] & G_2C = G_2A [05] ⇒  G_2 is midpoint of CA [15]
-003. G_1 is midpoint of CB [14] & G_2 is midpoint of CA [15] ⇒  G_1G_2 ∥ BA [16]
-004. G_1G_2 ∥ AB [16] & G_1,A,G are collinear [06] & G_2,B,G are collinear [07] ⇒  G_1G_2:G_1G = AB:AG [17]
-005. A,B,G_3 are collinear [12] & G_3A = G_3B [13] ⇒  G_3 is midpoint of BA [18]
-006. G_2 is midpoint of CA [15] & G_3 is midpoint of BA [18] ⇒  G_2G_3 ∥ CB [19]
-007. A,B,G_3 are collinear [12] & BC ∥ G_2G_3 [19] & AB ∥ G_1G_2 [16] ⇒  ∠G_3G_2G_1 = ∠G_2G_3A [20]
-008. G_1 is midpoint of CB [14] & G_3 is midpoint of BA [18] ⇒  G_1G_3 ∥ CA [21]
-009. G_2,A,C are collinear [04] & A,B,G_3 are collinear [12] & AC ∥ G_1G_3 [21] & AB ∥ G_1G_2 [16] ⇒  ∠G_3G_1G_2 = ∠G_2AG_3 [22]
-010. ∠G_3G_2G_1 = ∠G_2G_3A [20] & ∠G_3G_1G_2 = ∠G_2AG_3 [22] (Similar Triangles)⇒  G_2G_1 = G_3A [23]
-011. CA ⟂ H_2B [10] & BH ⟂ AC [00] & C,A,H_2 are collinear [11] ⇒  BH_2 ⟂ H_2A [24]
-012. BH_2 ⟂ H_2A [24] & G_3 is midpoint of BA [18] ⇒  BG_3 = H_2G_3 [25]
-013. G_1G_2:G_1G = AB:AG [17] & G_2G_1 = G_3A [23] & G_3A = G_3B [13] & BG_3 = H_2G_3 [25] ⇒  H_2G_3:G_1G = AB:AG [26]
-014. G_1B = G_1C [03] & OB = OC [08] ⇒  CB ⟂ G_1O [27]
-015. CB ⟂ G_1O [27] & AH ⟂ BC [01] & AB ∥ G_1G_2 [16] ⇒  ∠HAB = ∠OG_1G_2 [28]
-016. OA = OB [09] & OB = OC [08] ⇒  OC = OA [29]
-017. G_2C = G_2A [05] & OC = OA [29] ⇒  CA ⟂ G_2O [30]
-018. CA ⟂ G_2O [30] & BH ⟂ AC [00] & AB ∥ G_1G_2 [16] ⇒  ∠HBA = ∠OG_2G_1 [31]
-019. ∠HAB = ∠OG_1G_2 [28] & ∠HBA = ∠OG_2G_1 [31] (Similar Triangles)⇒  AB:HA = G_1G_2:G_1O [32]
-020. G_1G_2:G_1O = AB:HA [32] & G_2G_1 = G_3A [23] & G_3A = G_3B [13] & BG_3 = H_2G_3 [25] ⇒  H_2G_3:G_1O = AB:HA [33]
-021. H_2G_3:G_1G = AB:AG [26] & H_2G_3:G_1O = AB:HA [33] ⇒  AG:HA = G_1G:G_1O [34]
-022. G_1,A,G are collinear [06] & CB ⟂ G_1O [27] & AH ⟂ BC [01] ⇒  ∠GAH = ∠GG_1O [35]
-023. AG:HA = G_1G:G_1O [34] & ∠GAH = ∠GG_1O [35] (Similar Triangles)⇒  ∠AGH = ∠G_1GO [36]
-024. ∠AGH = ∠G_1GO [36] & G_1,A,G are collinear [06] ⇒  GH ∥ GO [37]
-025. GH ∥ GO [37] ⇒  O,H,G are collinear
-==========================
-`;
+//  * Proof steps:
+// 001. G_1,C,B are collinear [02] & G_1B = G_1C [03] ⇒  G_1 is midpoint of CB [14]
+// 002. C,A,G_2 are collinear [04] & G_2C = G_2A [05] ⇒  G_2 is midpoint of CA [15]
+// 003. G_1 is midpoint of CB [14] & G_2 is midpoint of CA [15] ⇒  G_1G_2 ∥ BA [16]
+// 004. G_1G_2 ∥ AB [16] & G_1,A,G are collinear [06] & G_2,B,G are collinear [07] ⇒  G_1G_2:G_1G = AB:AG [17]
+// 005. A,B,G_3 are collinear [12] & G_3A = G_3B [13] ⇒  G_3 is midpoint of BA [18]
+// 006. G_2 is midpoint of CA [15] & G_3 is midpoint of BA [18] ⇒  G_2G_3 ∥ CB [19]
+// 007. A,B,G_3 are collinear [12] & BC ∥ G_2G_3 [19] & AB ∥ G_1G_2 [16] ⇒  ∠G_3G_2G_1 = ∠G_2G_3A [20]
+// 008. G_1 is midpoint of CB [14] & G_3 is midpoint of BA [18] ⇒  G_1G_3 ∥ CA [21]
+// 009. G_2,A,C are collinear [04] & A,B,G_3 are collinear [12] & AC ∥ G_1G_3 [21] & AB ∥ G_1G_2 [16] ⇒  ∠G_3G_1G_2 = ∠G_2AG_3 [22]
+// 010. ∠G_3G_2G_1 = ∠G_2G_3A [20] & ∠G_3G_1G_2 = ∠G_2AG_3 [22] (Similar Triangles)⇒  G_2G_1 = G_3A [23]
+// 011. CA ⟂ H_2B [10] & BH ⟂ AC [00] & C,A,H_2 are collinear [11] ⇒  BH_2 ⟂ H_2A [24]
+// 012. BH_2 ⟂ H_2A [24] & G_3 is midpoint of BA [18] ⇒  BG_3 = H_2G_3 [25]
+// 013. G_1G_2:G_1G = AB:AG [17] & G_2G_1 = G_3A [23] & G_3A = G_3B [13] & BG_3 = H_2G_3 [25] ⇒  H_2G_3:G_1G = AB:AG [26]
+// 014. G_1B = G_1C [03] & OB = OC [08] ⇒  CB ⟂ G_1O [27]
+// 015. CB ⟂ G_1O [27] & AH ⟂ BC [01] & AB ∥ G_1G_2 [16] ⇒  ∠HAB = ∠OG_1G_2 [28]
+// 016. OA = OB [09] & OB = OC [08] ⇒  OC = OA [29]
+// 017. G_2C = G_2A [05] & OC = OA [29] ⇒  CA ⟂ G_2O [30]
+// 018. CA ⟂ G_2O [30] & BH ⟂ AC [00] & AB ∥ G_1G_2 [16] ⇒  ∠HBA = ∠OG_2G_1 [31]
+// 019. ∠HAB = ∠OG_1G_2 [28] & ∠HBA = ∠OG_2G_1 [31] (Similar Triangles)⇒  AB:HA = G_1G_2:G_1O [32]
+// 020. G_1G_2:G_1O = AB:HA [32] & G_2G_1 = G_3A [23] & G_3A = G_3B [13] & BG_3 = H_2G_3 [25] ⇒  H_2G_3:G_1O = AB:HA [33]
+// 021. H_2G_3:G_1G = AB:AG [26] & H_2G_3:G_1O = AB:HA [33] ⇒  AG:HA = G_1G:G_1O [34]
+// 022. G_1,A,G are collinear [06] & CB ⟂ G_1O [27] & AH ⟂ BC [01] ⇒  ∠GAH = ∠GG_1O [35]
+// 023. AG:HA = G_1G:G_1O [34] & ∠GAH = ∠GG_1O [35] (Similar Triangles)⇒  ∠AGH = ∠G_1GO [36]
+// 024. ∠AGH = ∠G_1GO [36] & G_1,A,G are collinear [06] ⇒  GH ∥ GO [37]
+// 025. GH ∥ GO [37] ⇒  O,H,G are collinear
+// ==========================
+// `;
 
 class proposition {
   readonly premises: number[];
@@ -94,6 +86,7 @@ class proposition {
 }
 
 class solution {
+  readonly id: string;
   readonly source: string;
   info: { [key: number]: string };
   tpinfo: number[];
@@ -102,7 +95,8 @@ class solution {
   acpoints: string[];
   steps: proposition[];
 
-  constructor(source: string) {
+  constructor(id: string, source: string) {
+    this.id = id;
     this.source = source;
     this.info = {};
     this.tpinfo = [];
@@ -113,8 +107,8 @@ class solution {
   }
 }
 
-function parse_solution(source: string): solution {
-  var sol = new solution(source);
+function parse_solution(id: string, source: string): solution {
+  var sol = new solution(id, source);
 
   source = source
     .replace("==========================", "")
@@ -220,7 +214,7 @@ export default function Solution({ params }: { params: { id: string } }) {
         if (result.content) {
           // console.log(result.content);
           setSol(result.content);
-          setPsol(parse_solution(result.content));
+          setPsol(parse_solution(params.id, result.content));
           // console.log(parse_solution(result.content));
           // console.log(psol);
         }
@@ -238,7 +232,13 @@ export default function Solution({ params }: { params: { id: string } }) {
           ) : (
             <>
               {/* <p>{sol}</p> */}
-              <ViewSol parsed_sol={psol} />
+              <div className="z-50">
+                <ViewSol parsed_sol={psol} />
+              </div>
+              <div className="w-[100vmin] h-[100vmin] p-6 absolute right-0 top-0 z-1">
+                <Graph sol={psol} />
+              </div>
+              <JSXGraphLib />
             </>
           )
         ) : (
@@ -251,205 +251,38 @@ export default function Solution({ params }: { params: { id: string } }) {
 
 // Graph View
 
-// export const Graph = ({ sol }: { sol: solution }): JSX.Element => {
-//   const [tooltipDisabled, setTooltipDisabled] = useState(false);
-//   const [openTextCopyModal, setOpenTextCopyModal] = useState(false);
-//   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-//   const { show, hideAll } = useContextMenu({ id: `CTXMenu-${sol.id}` });
-//   const graph = new JSXGraph(`jsxcanvas-${sol.id}`);
-//   sol.tppoints.forEach((v) =>
-//     graph.createPoint(
-//       v,
-//       new COORD(getRandomIntInclusive(-7, 7), getRandomIntInclusive(-7, 7)),
-//       { color: "blue" }
-//     )
-//   );
-//   sol.acpoints.forEach((v) =>
-//     graph.createPoint(
-//       v,
-//       new COORD(getRandomIntInclusive(-7, 7), getRandomIntInclusive(-7, 7)),
-//       { color: "orange" }
-//     )
-//   );
-//   graph.createPoint("p1", new COORD(1, 2));
-//   graph.createPoint("p2", new COORD(2, 2));
-//   graph.createPoint("p3", new COORD(2, 1));
-//   graph.createLine("l1", "p1", "p2");
-//   graph.createLine("l2", "p2", "p3");
-//   graph.createAngle("a1", "p1", "p2", "p3");
-//   function onContextMenu(event: React.MouseEvent<HTMLDivElement>): void {
-//     event.preventDefault();
-//     event.stopPropagation();
+export const Graph = ({ sol }: { sol: solution }): JSX.Element => {
+  const graph = new JSXGraph(`jsxcanvas-${sol.id}`);
+  sol.tppoints.forEach((v) =>
+    graph.createPoint(
+      v,
+      new COORD(getRandomIntInclusive(-7, 7), getRandomIntInclusive(-7, 7)),
+      { color: "blue" }
+    )
+  );
+  sol.acpoints.forEach((v) =>
+    graph.createPoint(
+      v,
+      new COORD(getRandomIntInclusive(-7, 7), getRandomIntInclusive(-7, 7)),
+      { color: "orange" }
+    )
+  );
+  graph.createPoint("p1", new COORD(1, 2));
+  graph.createPoint("p2", new COORD(2, 2));
+  graph.createPoint("p3", new COORD(2, 1));
+  graph.createLine("l1", "p1", "p2");
+  graph.createLine("l2", "p2", "p3");
+  graph.createAngle("a1", "p1", "p2", "p3");
 
-//     if (event.target === null) return;
-//     if (!(event.target instanceof HTMLElement)) return;
-//     var target = getThesisDiv(event.target);
-
-//     hideAll();
-//     target !== null &&
-//       show({
-//         event,
-//         props: {
-//           key: "value",
-//         },
-//       });
-//   }
-
-//   const handleItemClick = ({ id, event, props, data }: ItemParams) => {
-//     switch (id) {
-//       case "copy":
-//         console.log(event, props);
-//         break;
-//       case "cut":
-//         console.log(event, props);
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   var toPointName: (x: string) => string = (x): string =>
-//     x[0] + (x.slice(1) ? "_" : "") + x.slice(1);
-
-//   return (
-//     <>
-//       <div style={{ width: "100%", display: "flex", justifyContent: "left" }}>
-//         <JSXGraphCanvas
-//           id={`jsxcanvas-${sol.id}`}
-//           style={{ width: "800px", height: "800px" }}
-//         />
-//         <div className="SolutionDiv" onContextMenu={onContextMenu}>
-//           <div className="title">
-//             <h1>Theorem Premise</h1>
-//           </div>
-//           {
-//             build_thesis_hint(sol.tppoints.map(toPointName).join(", ")).props
-//               .children
-//           }
-//           {sol.tpinfo.map((n) => build_info_view(sol, n))}
-//           <div className="title">
-//             <h1>Auxiliary Construction</h1>
-//           </div>
-//           {
-//             build_thesis_hint(sol.acpoints.map(toPointName).join(", ")).props
-//               .children
-//           }
-//           {sol.acinfo.map((n) => build_info_view(sol, n))}
-//           <div className="title">
-//             <h1>Proof Step</h1>
-//           </div>
-//           {sol.steps.map((pro) => build_step_view(sol, pro))}
-//           <Tooltip
-//             id={`solution-tooltip-${sol.id}`}
-//             place="right"
-//             hidden={tooltipDisabled}
-//           />
-//           {/* <Tooltip id={`solution-ctxmenu-${sol.id}`} place="right" clickable /> */}
-//         </div>
-//       </div>
-
-//       <button
-//         className="btn-outline"
-//         onClick={() => setOpenTextCopyModal(true)}
-//       >
-//         <span>Copy Text</span>
-//       </button>
-//       {/* <button className="btn-outline" onClick={() => window.navigator.clipboard.writeText(sol.source).then(() => {closeSnackbar(); enqueueSnackbar("Copied Source", {variant:"info"});} )}><span>Copy Source</span></button> */}
-//       <button
-//         className="btn-outline"
-//         onClick={() => {
-//           const blob = new Blob([sol.source], { type: "text/plain" });
-//           const url = window.URL.createObjectURL(blob);
-
-//           const a = document.createElement("a");
-//           a.style.display = "none";
-//           a.href = url;
-//           a.download = "solution-" + sol.id + ".txt";
-//           document.body.appendChild(a);
-
-//           a.click();
-
-//           setTimeout(() => {
-//             document.body.removeChild(a);
-//             window.URL.revokeObjectURL(url);
-//             closeSnackbar();
-//             enqueueSnackbar("Copied Source", { variant: "info" });
-//           }, 100);
-//         }}
-//       >
-//         <span>Download Source</span>
-//       </button>
-
-//       <div>
-//         <label style={{ display: "flex", whiteSpace: "pre" }}>
-//           <Toggle
-//             onChange={(event) => setTooltipDisabled(!event.target.checked)}
-//             defaultChecked={true}
-//           ></Toggle>{" "}
-//           Enable Thesis Hint
-//         </label>
-//       </div>
-
-//       <Menu id={`CTXMenu-${sol.id}`}>
-//         <Item id="copy" onClick={handleItemClick}>
-//           Copy
-//         </Item>
-//         <Item id="cut" onClick={handleItemClick}>
-//           Cut
-//         </Item>
-//         <Separator />
-//         <Item disabled>Disabled</Item>
-//         <Separator />
-//         <Submenu label="Foobar">
-//           <Item id="reload" onClick={handleItemClick}>
-//             Reload
-//           </Item>
-//           <Item id="something" onClick={handleItemClick}>
-//             Do something else
-//           </Item>
-//         </Submenu>
-//       </Menu>
-
-//       <ReactModal
-//         isOpen={openTextCopyModal}
-//         appElement={undefined}
-//         onRequestClose={() => setOpenTextCopyModal(false)}
-//       >
-//         <div className="title">
-//           <h1>Main Text</h1>
-//         </div>
-//         <span style={{ whiteSpace: "pre-wrap" }}>
-//           {" * From theorem premises:"}
-//           <br />
-//           {sol.tppoints.map(toPointName).join(", ")}
-//           <br />
-//           {sol.tpinfo.map((n) => `[${n + 1}] ` + sol.info[n]).join("\n")}
-//           <br />
-//           <br />
-//           {" * Auxiliary Constructions:"}
-//           <br />
-//           {sol.acpoints.map(toPointName).join(", ")}
-//           <br />
-//           {sol.acinfo.map((n) => `[${n + 1}] ` + sol.info[n]).join("\n")}
-//           <br />
-//           <br />
-//           {" * Proof steps:"}
-//           <br />
-//           {sol.steps
-//             .map(
-//               (pro) =>
-//                 pro.premises
-//                   .map((n) => `[${n + 1}] ${sol.info[n]}`)
-//                   .join(" & ") +
-//                 (pro.conclusion < 0 ? " ⇒" : ` ⇒ [${pro.conclusion + 1}]`) +
-//                 ` ${sol.info[pro.conclusion]}`
-//             )
-//             .join("\n")}
-//         </span>
-//       </ReactModal>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <JSXGraphCanvas
+        id={`jsxcanvas-${sol.id}`}
+        style={{ width: "100%", height: "100%" }}
+      />
+    </>
+  );
+};
 
 // Sol Side Bar Code
 
@@ -461,8 +294,8 @@ export const ViewSol = ({ parsed_sol }: { parsed_sol: solution }) => {
     <div
       className={
         isSolBarOpened
-          ? `flex fixed h-full left-[0px] top-0 items-top`
-          : `flex fixed h-full left-[-460px] top-0 items-top`
+          ? `flex fixed h-full left-[0px] top-0 items-top bg-white`
+          : `flex fixed h-full left-[-460px] top-0 items-top bg-white`
       }
     >
       <div className="w-[450px] h-full p-8  overflow-y-scroll overflow-x-visible">
--- src/shared/components/JsxGraph.tsx
+++ src/shared/components/JsxGraph.tsx
@@ -1,176 +1,228 @@
 import React, { CSSProperties } from "react";
-import useScript from "../hooks/useScript.tsx";
-import useLink from "../hooks/useLink.tsx";
-import { COORD } from "../../types/util/DataUtil.tsx";
+import useScript from "../hooks/useScript";
+import useLink from "../hooks/useLink";
+import { COORD } from "../../utils/types/DataUtil.jsx";
 
 let onJSXGraphLoaded: (() => void)[] = [];
 
 export const JSXGraphLib = () => {
-    
-    useLink("https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraph.css", "stylesheet", "text/css");
-    useScript("https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraphcore.js", false, () => onJSXGraphLoaded.forEach((f) => f()));
-    // useScript("https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js");
-    return <></>;
-}
+  useLink(
+    "https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraph.css",
+    "stylesheet",
+    "text/css"
+  );
+  useScript(
+    "https://cdn.jsdelivr.net/npm/jsxgraph/distrib/jsxgraphcore.js",
+    false,
+    () => onJSXGraphLoaded.forEach((f) => f())
+  );
+  // useScript("https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js");
+  return <></>;
+};
 
-export const JSXGraphCanvas = ( {id, style}: {id: string, style?: CSSProperties | undefined} ) => { return <div id={id} className="jxgbox" style={style} />; }
+export const JSXGraphCanvas = ({
+  id,
+  style,
+}: {
+  id: string;
+  style?: CSSProperties | undefined;
+}) => {
+  return <div id={id} className="jxgbox" style={style} />;
+};
 
 interface JSXStyle {}
 
 interface JSXPointStyle extends JSXStyle {
-    color?: string | undefined;
+  color?: string | undefined;
 }
 
 interface JSXLineStyle extends JSXStyle {
-    straightFirst?: boolean | undefined;
-    straightLast?: boolean | undefined;
+  straightFirst?: boolean | undefined;
+  straightLast?: boolean | undefined;
 }
 
-interface JSXAngleStyle extends JSXStyle {
-
-}
+interface JSXAngleStyle extends JSXStyle {}
 
 function styleMap<T>(name: string, defaultStyle: T, style: T): Object {
-    return Object.assign({name: name}, defaultStyle, style);
+  return Object.assign({ name: name }, defaultStyle, style);
 }
 
 interface JSXObject {
-    getObject: () => any;
-    setAttribute: (style: JSXStyle) => void;
+  getObject: () => any;
+  setAttribute: (style: JSXStyle) => void;
 }
 
-
 class JSXPoint implements JSXObject {
+  jsxobj: any;
+  constructor(jsxobj: any) {
+    this.jsxobj = jsxobj;
+  }
 
-    jsxobj: any;
-    constructor(jsxobj: any) {
-        this.jsxobj = jsxobj;
-    }
-
-    getObject: () => any = () => this.jsxobj;
-    setAttribute: (style: JSXPointStyle) => void = style => this.jsxobj.setAttribute(style);
+  getObject: () => any = () => this.jsxobj;
+  setAttribute: (style: JSXPointStyle) => void = (style) =>
+    this.jsxobj.setAttribute(style);
 }
 
 class JSXLine implements JSXObject {
+  jsxobj: any;
+  constructor(jsxobj: any) {
+    this.jsxobj = jsxobj;
+  }
 
-    jsxobj: any;
-    constructor(jsxobj: any) {
-        this.jsxobj = jsxobj;
-    }
-
-    getObject: () => any = () => this.jsxobj;
-    setAttribute: (style: JSXLineStyle) => void = style => this.jsxobj.setAttribute(style);
+  getObject: () => any = () => this.jsxobj;
+  setAttribute: (style: JSXLineStyle) => void = (style) =>
+    this.jsxobj.setAttribute(style);
 }
 
 class JSXAngle implements JSXObject {
+  jsxobj: any;
+  constructor(jsxobj: any) {
+    this.jsxobj = jsxobj;
+  }
 
-    jsxobj: any;
-    constructor(jsxobj: any) {
-        this.jsxobj = jsxobj;
-    }
-
-    getObject: () => any = () => this.jsxobj;
-    setAttribute: (style: JSXAngleStyle) => void = style => this.jsxobj.setAttribute(style);
+  getObject: () => any = () => this.jsxobj;
+  setAttribute: (style: JSXAngleStyle) => void = (style) =>
+    this.jsxobj.setAttribute(style);
 }
 
 function getJSXObjectClass(typename: string) {
-    switch (typename) {
-        case "point": return JSXPoint;
-        case "line": return JSXLine;
-        case "angle": return JSXAngle;
-        default: return null;
-    }
+  switch (typename) {
+    case "point":
+      return JSXPoint;
+    case "line":
+      return JSXLine;
+    case "angle":
+      return JSXAngle;
+    default:
+      return null;
+  }
 }
 
 export class JSXGraph {
-    private readonly id: string;
+  private readonly id: string;
 
-    private board: any = undefined;
-    private typeDict: { [x: string]: string[] };
-    private itemDict: { [x: string]: JSXObject; };
+  private board: any = undefined;
+  private typeDict: { [x: string]: string[] };
+  private itemDict: { [x: string]: JSXObject };
 
-    private onCanvasReady: (() => void)[]
-    constructor(id: string) {
-        this.id = id;
+  private onCanvasReady: (() => void)[];
+  constructor(id: string) {
+    this.id = id;
 
-        this.itemPoints = [];
-        this.typeDict = {
-            "point": [],
-            "line": [],
-            "angle": [],
-        };
-        this.itemDict = {};
+    this.itemPoints = [];
+    this.typeDict = {
+      point: [],
+      line: [],
+      angle: [],
+    };
+    this.itemDict = {};
 
-        this.onCanvasReady = [];
+    this.onCanvasReady = [];
 
-        if (window.JXG) this.init();
-        else onJSXGraphLoaded.push(this.init);
+    if (window.JXG) this.init();
+    else onJSXGraphLoaded.push(this.init);
+  }
+  init = () => {
+    var canvasDiv = document.getElementById(this.id);
+    if (!canvasDiv) {
+      console.warn("Failed to find canvas. Retry...");
+      setTimeout(this.init, 100);
+      return;
     }
-    init = () => {
-        var canvasDiv = document.getElementById(this.id);
-        if (!canvasDiv) { console.warn("Failed to find canvas. Retry..."); setTimeout(this.init, 100); return; }
-        canvasDiv.innerHTML = "";
-        this.board = window.JXG.JSXGraph.initBoard(this.id, {boundingbox: [-8, 8, 8, -8]});
-        this.onCanvasReady.forEach(f => f())
+    canvasDiv.innerHTML = "";
+    this.board = window.JXG.JSXGraph.initBoard(this.id, {
+      boundingbox: [-8, 8, 8, -8],
+    });
+    this.onCanvasReady.forEach((f) => f());
 
-        window.g = this;
+    window.g = this;
+  };
+  private execute = (fn: () => void) => {
+    if (this.board) {
+      fn();
+      return;
     }
-    private execute = (fn: () => void) => {
-        if (this.board) { fn(); return; }
-        if (window.JXG) { this.onCanvasReady.push(fn); }
-        onJSXGraphLoaded.push(fn);
+    if (window.JXG) {
+      this.onCanvasReady.push(fn);
     }
+    onJSXGraphLoaded.push(fn);
+  };
 
-    private get = (id: string) => this.itemDict[id];
+  private get = (id: string) => this.itemDict[id];
 
-    private create = (type: string, id: string, position, defaultStyle, style) => {
-        this.execute(() => {
-            var classobj = getJSXObjectClass(type);
-            if (classobj == null) { console.error("Failed to find jsx element " + type); return; }
+  private create = (
+    type: string,
+    id: string,
+    position,
+    defaultStyle,
+    style
+  ) => {
+    this.execute(() => {
+      var classobj = getJSXObjectClass(type);
+      if (classobj == null) {
+        console.error("Failed to find jsx element " + type);
+        return;
+      }
 
-            var item  = this.board.create(type, position, styleMap(id, defaultStyle, style));
-            var obj = new classobj(item);
-            this.itemDict[id] = obj;
-            this.typeDict[type].push(id)
-        });
+      var item = this.board.create(
+        type,
+        position,
+        styleMap(id, defaultStyle, style)
+      );
+      var obj = new classobj(item);
+      this.itemDict[id] = obj;
+      this.typeDict[type].push(id);
+    });
+  };
+
+  require = (idList: string[], callback: (...args: string[]) => void) => {
+    for (var id of idList) {
+      if (!this.itemDict[id]) {
+        setTimeout(() => this.require(idList, callback));
+        return;
+      }
     }
+    console.log(idList.map((x) => this.itemDict[x]));
+    callback(...idList.map((x) => this.itemDict[x].getObject()));
+  };
 
-    require = (idList: string[], callback: (...args: string[]) => void) => {
-        for (var id of idList) {
-            if (!this.itemDict[id]) {
-                setTimeout(() => this.require(idList, callback));
-                return;
-            }
-        }
-        console.log(idList.map(x => this.itemDict[x]))
-        callback(...idList.map(x => this.itemDict[x].getObject()))
+  // createPoint = (id: string, position: COORD, style: JSXPointStyle = {}) => this.itemDict[id] = this.create('point', id, position.toArray(), {}, style)
+  // createLine = (id: string, p1: string, p2: string, style: JSXLineStyle = {}) => {
+  //     this.create('line', id, [p1, p2], { straightFirst: false, straightLast: false }, style)
+  // }
+  // createAngle = (id: string, p1: string, p2: string, p3: string, style: JSXAngleStyle = {}) => this.create('angle', id, [p1, p2, p3], {}, style)
 
-    }
-
-    // createPoint = (id: string, position: COORD, style: JSXPointStyle = {}) => this.itemDict[id] = this.create('point', id, position.toArray(), {}, style)
-    // createLine = (id: string, p1: string, p2: string, style: JSXLineStyle = {}) => {
-    //     this.create('line', id, [p1, p2], { straightFirst: false, straightLast: false }, style)
-    // }
-    // createAngle = (id: string, p1: string, p2: string, p3: string, style: JSXAngleStyle = {}) => this.create('angle', id, [p1, p2, p3], {}, style)
-
-    createPoint = (id: string, position: COORD, style: JSXPointStyle = {}) => {
-        this.execute(() => {
-            var item = this.board.create('point', position.toArray(), styleMap(id, {}, style));
-            this.itemDict[id] = new JSXPoint(item);
-            this.typeDict['point'].push(id);
-        });
-    }
-    createLine = (id: string, p1: string, p2: string, style: JSXLineStyle = {}) =>
-        this.require(
-            [p1, p2],
-            (...objList) => this.create('line', id, objList, { straightFirst: false, straightLast: false }, style)
-        )
-    createAngle = (id: string, p1: string, p2: string, p3: string, style: JSXAngleStyle = {}) => 
-        this.require(
-            [p1, p2, p3],
-            (...objList) => this.create('angle', id, objList, {}, style)
-        )
-
+  createPoint = (id: string, position: COORD, style: JSXPointStyle = {}) => {
+    this.execute(() => {
+      var item = this.board.create(
+        "point",
+        position.toArray(),
+        styleMap(id, {}, style)
+      );
+      this.itemDict[id] = new JSXPoint(item);
+      this.typeDict["point"].push(id);
+    });
+  };
+  createLine = (id: string, p1: string, p2: string, style: JSXLineStyle = {}) =>
+    this.require([p1, p2], (...objList) =>
+      this.create(
+        "line",
+        id,
+        objList,
+        { straightFirst: false, straightLast: false },
+        style
+      )
+    );
+  createAngle = (
+    id: string,
+    p1: string,
+    p2: string,
+    p3: string,
+    style: JSXAngleStyle = {}
+  ) =>
+    this.require([p1, p2, p3], (...objList) =>
+      this.create("angle", id, objList, {}, style)
+    );
 }
 
 // const useJSXGraph = (id: string) => {
